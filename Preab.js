aa0394d6515b5545b49f2725e27cc7cbcce9c7ed
Engr. Emmanuel Umoh <98795838+Nuelou@users.noreply.github.com>
2022-04-18 19:23:18 +0100

    Time, Date and Position added
    Yet to use one button to render the 3 variables

--- Preab.js
+++ Preab.js
@@ -1,36 +1,32 @@
-var x = document.getElementById("demo");
-
-function getLocation() {
-    if (navigator.geolocation) {
-        x.innerHTML = navigator.geolocation.getCurrentPosition(showPosition, noPosition, {timeout:10000});
-        console.log(x.innerHTML)
-    } else {
-        console.log("theme")
-        x.innerHTML = "Geolocation is not supported by this browser.";
+// Return Longitude and Latitude of user's position
+// var x = document.getElementById("Geolocation");
+//     function getLocation() {
+//       if (navigator.geolocation) {
+//             navigator.geolocation.getCurrentPosition(showPosition);
+//           } else {
+//             x.innerHTML = "Geolocation is not supported by this browser.";
+//           }
+//         }
         
-    }
-}
-// console.log(getLocation)
+        // function showPosition(position) {
+        //   x.innerHTML = "Latitude: " + position.coords.latitude +
+        //   "<br>Longitude: " + position.coords.longitude;
+        // }
+        // function showPosition(position) {
+        //     x.innerHTML = "Latitude: " + position.coords.accuracy +
+        //     "<br>Longitude: " + position.coords.accuracy;
+        //   }
 
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-}
+          var x = document.getElementById("Geolocation");
+    function getLocation() {
+      if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(showPosition);
+          } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+          }
+        }
 
-function noPosition() {
-    $("#demo").watermark("Could not find location");
-  }
-
-// function display_ct5() {
-//     var x = new Date()
-//     var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
-//     var x1=x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getFullYear();
-//     x1 = x1 + " - " + x.getHours( )+ ":" + x.getMinutes() + ":" + x.getSeconds() + ":" + ampm;
-//     document.getElementsById('ct5').innerHTML = x1;
-//     display_ct5();
-// }
-// function display_ct5() {
-//     var refresh=1000; // Refresh rate in milli seconds
-//     mytime=setTimeout('display_ct5()',refresh)
-// }
-// display_ct5
-
+        function showPosition(position) {
+            x.innerHTML = "Latitude: " + position.coords.accuracy +
+            "<br>Longitude: " + position.coords.accuracy;
+          }
